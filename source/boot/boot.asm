cf0a82850cfb8b3edb06f3c0258a604d22ad0851
aowei <aoweichen@gmail.com>
2025-07-05 14:53:44 +0800

    更正汇编模式，将汇编代码改为标准一点的汇编代码

--- source/boot/boot.asm
+++ source/boot/boot.asm
@@ -1,44 +1,62 @@
+; 指定加载地址为 0x7c00
 [org 0x7c00]
+; 指定当前为 16 位实模式
+bits 16
 
-; 设置屏幕模式为文本模式，清除屏幕
-mov ax, 3
-int 0x10
+; ===== 数据段（存放已初始化的全局数据）=====
+section .data
 
-; 初始化段寄存器（不初始化的话某些虚拟机上会有问题）
-mov ax, 0
-mov ds, ax
-mov es, ax
-mov ss, ax
-mov sp, 0x7c00
 
-; bochs 的魔数断点
-xchg bx, bx
+; ===== BSS段（预留未初始化内存空间）=====
+; 本程序暂时没有未初始化的内存空间
+section .bss
 
-mov si, booting
-call print
 
-; 阻塞
-jmp $
+; ===== 代码段（程序指令）=====
+section .text
+global _start
+_start:
+    ; 初始化段寄存器，在实模式下必须设置，否则在某些虚拟机上会有问题
+    ; 将 ax 置 0，将段寄存器都置 0
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    ; 初始化栈顶指针，栈指针指向 0x7c00 （向下增长）
+    mov sp, 0x7c00
 
+    ; 设置文本模式并清除屏幕
+    mov ax, 3
+    int 0x10
+
+    ; Bochs 调试断点魔数（magic breakpoint）
+    xchg bx, bx
+    ; 调用打印函数
+    mov si, booting_msg
+    call print
+
+    ; 阻塞
+    jmp $
+
+; 初始化字符串
+booting_msg db "Booting AWOS...", 10, 13, 0
+
+; ===== 子函数：打印字符串（SI=字符串地址）=====
 print:
-    mov ah,0x0e
-
-.next:
+    mov ah, 0x0e
+.loop:
     mov al, [si]
     cmp al, 0
     jz .done
     int 0x10
     inc si
-    jmp .next
+    jmp .loop
 .done:
     ret
 
-
-booting:
-    db "Booting AWOS...", 10, 13, 0; \n, \r, 0 表示字符串结束
-
-
+; ===== 填充引导扇区结束标记 =====
 ; 填充 0
 times 510 - ($ - $$) db 0
 ; 硬盘校验码，主引导扇区的最后两个字节必须是 0x55 0xaa
+; 小端序：0x55 0xAA
 db 0x55, 0xaa
