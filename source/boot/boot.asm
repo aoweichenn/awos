6e1977f485aff97bff5f712e506974d9810ee1ac
aowei <aoweichen@gmail.com>
2025-07-05 13:53:05 +0800

    005 实模式 print

--- source/boot/boot.asm
+++ source/boot/boot.asm
@@ -11,13 +11,32 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-; 0xb800 是文本显示器的内存区域
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+; bochs 的魔数断点
+xchg bx, bx
+
+mov si, booting
+call print
 
 ; 阻塞
 jmp $
+
+print:
+    mov ah,0x0e
+
+.next:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .next
+.done
+    ret
+
+
+booting:
+    db "Booting AWOS...", 10, 13, 0; \n, \r, 0 表示字符串结束
+
 
 ; 填充 0
 times 510 - ($ - $$) db 0
