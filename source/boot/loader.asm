88135d6be0841c2263d98175abfc8d65d8db2f65
aowei <aoweichen@gmail.com>
2025-07-06 20:32:18 +0800

    008 保护模式和全局描述符

--- source/boot/loader.asm
+++ source/boot/loader.asm
@@ -2,48 +2,8 @@
 ; 魔数 0x55aa，用于判断有无错误
 dw 0x55aa
 
-section .text
-
 mov si, loading_msg
 call print
-call detect_memory
-
-; 阻塞
-jmp $
-
-
-
-
-
-loading_msg:
-    db "Loading AWOS...", 10, 13, 0
-loading_error_msg:
-    db "Loading Error!!!", 10, 13, 0
-detecting_memory_success_msg:
-    db "Detecting Memory Success...", 10, 13, 0
-
-; ====== 子函数模块 ======
-; 处理加载错误的函数
-handle_loading_error:
-    mov si, loading_error_msg
-    call print
-    ; 让 CPU 停止
-    hlt
-    ; 阻塞
-    jmp $
-
-; 打印函数，将 si 中的信息打印出来
-print:
-    mov ah, 0x0e
-.loop:
-    mov al, [si]
-    cmp al, 0
-    jz .done
-    int 0x10
-    inc si
-    jmp .loop
-.done:
-    ret
 
 ; 内存检测
 detect_memory:
@@ -69,7 +29,7 @@ detect_memory:
         ; 将缓存指针指向下一个结构体
         add di, cx
         ; 将结构体数量 + 1
-        inc word [ards_count]
+        inc dword [ards_count]
 
         cmp ebx, 0
         jnz .next
@@ -77,21 +37,114 @@ detect_memory:
         mov si, detecting_memory_success_msg
         call print
 
-        xchg bx, bx
-        ; 结构体数量
-        mov cx, [ards_count]
-        ; 结构体指针
-        mov si, 0
+        jmp prepare_protected_mode
 
-        .show:
-            mov eax, [si + ards_buffer]
-            mov ebx, [si + ards_buffer + 8]
-            mov edx, [si + ards_buffer + 16]
-            add si, 20
-            xchg bx, bx
-            loop .show
+prepare_protected_mode:
+    cli; 关闭中断
 
+    ; 打开 a20 线
+    in al, 0x92
+    or al, 0b10
+    out 0x92, al
+
+    ; 加载 gdt
+    lgdt [gdt_ptr]
+
+    ; 启用保护模式
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+    xchg bx, bx
+    ; 用跳转来刷新缓存, 并启用保护模式
+    jmp dword code_selector:protect_mode
+
+; 打印函数，将 si 中的信息打印出来
+print:
+    mov ah, 0x0e
+.loop:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .loop
+.done:
+    ret
+
+; 处理加载错误的函数
+handle_loading_error:
+    mov si, loading_error_msg
+    call print
+    ; 让 CPU 停止
+    hlt
+    ; 阻塞
+    jmp $
+
+
+loading_msg:
+    db "Loading AWOS...", 10, 13, 0
+loading_error_msg:
+    db "Loading Error!!!", 10, 13, 0
+detecting_memory_success_msg:
+    db "Detecting Memory Success...", 10, 13, 0
+
+
+
+[bits 32]
+protect_mode:
+    mov ax, data_selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, 0x10000; 修改栈顶
+    xchg bx, bx
+    mov byte [0xb8000], 'P'
+    mov byte [0x200000], 'p'
+
+jmp $
+
+code_selector equ (1 << 3)
+data_selector equ (2 << 3)
+
+memory_base equ 0; 内存开始的位置：基地址
+; 内存界限 (4G / 4k) -1
+memory_limit equ (((1024*1024*1024*4) / (1024*4)) - 1)
+
+
+gdt_ptr:
+    dw (gdt_end - gdt_base) - 1
+    dd gdt_base
+gdt_base:
+    dd 0, 0; NULL 描述符
+gdt_code:
+    ; 段界限的 0 ~ 15 位
+    dw memory_limit & 0xffff
+    ; 基地址 0 ~ 15
+    dw memory_base & 0xffff
+    ; 基地址的 16 ~ 23 位
+    db (memory_base >> 16) & 0xff
+    ; 存在 - dlp 0 - S - 代码 - 非依从 - 可读 - 没有被访问过
+    db 0b_1_00_1_1_0_1_0
+    ; 4k - 32 位 - 不是 64 位 - 段界限 16 ~ 19
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+    db (memory_base >> 24) & 0xff;
+gdt_data:
+    ; 段界限的 0 ~ 15 位
+    dw memory_limit & 0xffff
+    ; 基地址 0 ~ 15
+    dw memory_base & 0xffff
+    ; 基地址的 16 ~ 23 位
+    db (memory_base >> 16) & 0xff
+    ; 存在 - dlp 0 - S - 数据 - 向上 - 可写 - 没有被访问过
+    db 0b_1_00_1_0_0_1_0
+    ; 4k - 32 位 - 不是 64 位 - 段界限 16 ~ 19
+    db 0b1_1_0_0_0000 | (memory_limit >> 16) & 0xf
+    db (memory_base >> 24) & 0xff;
+gdt_end:
 
 ards_count:
-    dw 0
+    dd 0
 ards_buffer:
